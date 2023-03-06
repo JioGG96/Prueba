1ded586e31e18de35ed7605988f9648c75faaa2d
JioGG96 <gonzalez.granados.jovana@gmail.com>
2023-03-06 11:45:21 -0600

    Initial Commit

---
+++ JGG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JGG
+//
+//  Created by iOS UNAM 04 on 06/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
